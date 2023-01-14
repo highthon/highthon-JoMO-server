1baff138e6e370c98cda959d1c9c4139d02d67a3
이지민 <wlals060330@gmail.com>
2023-01-14 20:06:07 +0900

    init

---
+++ src/test/java/project/highthon/JoMO/JoMoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.highthon.JoMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoMoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
